46a0f462fa36c0cd9196ab242bd2d01eefcd4594
freddie0621 <freddiebay@gmail.com>
2018-05-09 00:52:51 +0200

    Add files via upload

--- SANITYFILE.py
+++ SANITYFILE.py
@@ -10,24 +10,26 @@ parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",
                   help="write report to FILE", metavar="FILE")
 (options, args) = parser.parse_args()
-print options.filename
+#print options.filename
 
 # Which file are we working with?
 fname = options.filename
 
-
+import re
+#print re.sub(r'[^\x00-\x7f]',r'_', fname)
 
 
 #print fname
 #sys.exit(fname)
-table = maketrans("'Ã´Ã ,Ã§Ã¦Ã©Ã¨ÃªÃ«'-?!: ","_oa_caeeee______")
+#table = maketrans("'ôà,çæéèêë'-?!: ","_oa_caeeee______")
 
 
 
-new_name= fname.translate(table,'').lower().replace(u'?','oe').replace('&','_')
+#new_name= fname.translate(table,'').lower().replace(u'?','oe').replace('&','_')
+#new_name= re.sub(r'[^\x00-\x7f]',r'_', new_name)
+fname1=fname.replace(' ','_').replace('é','e').replace('è','e').replace('î','i').replace('ê','e').replace('â','a').replace('ç','c').replace('\'','_')
+new_name = ''.join([s for s in fname1 if s in '_.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'])
 
 #sys.exit(new_name)
-
+print new_name
 rename(fname,new_name)
-
-
